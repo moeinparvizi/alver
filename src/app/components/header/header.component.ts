4a626204f123ea0f54fab4cb63e8dfed1e9fb6e5
moeinparvizi <giventulip@gmail.com>
2024-08-14 17:08:26 +0330

    some work on navigation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,37 @@
-import { Component, Injector, OnInit } from '@angular/core';
+import { Component, HostListener, Injector, OnInit } from '@angular/core';
 import { BaseComponent } from '../../base.component';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatSidenavModule, MatButtonModule, MatIconModule],
   styleUrl: './header.component.scss',
   templateUrl: './header.componenet.html',
 })
 export class HeaderComponent extends BaseComponent implements OnInit {
+  isSticky = false;
+  showFiller = false;
+
   constructor(injector: Injector) {
     super(injector);
-   }
+  }
 
- }
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const offset =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+
+    if (offset > 300) {
+      this.isSticky = true;
+    } else {
+      this.isSticky = false;
+    }
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1da1f2',
+        secondary: '#14171a',
+        accent: '#ffad1f',
+        white: '#ffffff',
+        black: '#000000',
+        gray: '#74706B',
+      },
+    },
   },
   plugins: [],
-}
-
+};
