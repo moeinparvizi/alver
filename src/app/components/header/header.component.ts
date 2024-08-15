2f77de669a41f639f71c18d017f56479d619ef36
moeinparvizi <giventulip@gmail.com>
2024-08-15 22:24:16 +0330

    create a false navigation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,20 +1,62 @@
-import { Component, HostListener, Injector, OnInit } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  Injector,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { BaseComponent } from '../../base.component';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
+import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MatSidenavModule, MatButtonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatMenuModule,
+  ],
   styleUrl: './header.component.scss',
   templateUrl: './header.componenet.html',
 })
 export class HeaderComponent extends BaseComponent implements OnInit {
   isSticky = false;
   showFiller = false;
+
+  menuSections = [
+    {
+      title: 'Section 1',
+      items: [
+        { name: 'Item 1', link: '#' },
+        { name: 'Item 2', link: '#' },
+        { name: 'Item 3', link: '#' },
+      ]
+    },
+    {
+      title: 'Section 2',
+      items: [
+        { name: 'Item 4', link: '#' },
+        { name: 'Item 5', link: '#' },
+        { name: 'Item 6', link: '#' },
+      ]
+    },
+    {
+      title: 'Section 3',
+      items: [
+        { name: 'Item 7', link: '#' },
+        { name: 'Item 8', link: '#' },
+        { name: 'Item 9', link: '#' },
+      ]
+    },
+  ];
+
+  @ViewChild('drawer') drawer!: MatDrawer;
 
   constructor(injector: Injector) {
     super(injector);
