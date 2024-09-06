ec0153148165cea58196dfd1d258e6c147b55b34
Moein parvizi <giventulip@gmail.com>
2024-09-06 15:37:54 +0330

    get a product

--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,5 +1,7 @@
+import { GetProductsService } from './../../services/getProducts/get-products.service';
 import { Component, Injector, OnInit } from '@angular/core';
 import { BaseComponent } from '../../base.component';
+import { ProductResponse } from '../../models/data.response';
 
 @Component({
   selector: 'app-product-detail',
@@ -9,15 +11,39 @@ import { BaseComponent } from '../../base.component';
   styleUrl: './product-detail.component.scss',
 })
 export class ProductDetailComponent extends BaseComponent implements OnInit {
-  constructor(injector: Injector) {
+  product?: ProductResponse;
+  isLoading = true;
+
+  constructor(
+    injector: Injector,
+    private getProductsService: GetProductsService
+  ) {
     super(injector);
   }
 
   override ngOnInit(): void {
     super.ngOnInit();
+
+    this.loadOnline();
   }
 
   override loadOnline(): void {
     super.loadOnline();
+
+    const productId = this.ActiveRoute.snapshot.paramMap.get('id');
+    const id = Number(productId);
+
+    this.getProductsService.getProduct(id).subscribe({
+      next: ({ product, isLoading }) => {
+        this.product = product;
+        this.isLoading = isLoading;
+      },
+      error: err => {
+        console.error('Error fetching products:', err);
+        this.isLoading = false;
+      },
+    });
+
+    console.log(this.product);
   }
 }
